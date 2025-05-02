c872f14e445cbc8487d493237b417ae1aa411064
srisista <sri.sista@gmail.com>
2025-05-02 22:18:06 +0530

    Git Action configured

--- src/main/java/com/tradestore/TradeStoreApplication.java
+++ src/main/java/com/tradestore/TradeStoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class TradeStoreApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TradeStoreApplication.class, args);
     }
